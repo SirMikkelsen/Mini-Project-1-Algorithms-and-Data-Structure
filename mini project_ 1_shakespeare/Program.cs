34003578ce12240e84606d9636b7f83d40bfde41
Gordon <gordonmikkelsen87@gmail.com>
2020-03-01 11:38:45 +0100

    Create project

---
+++ mini project_ 1_shakespeare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mini_project__1_shakespeare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
