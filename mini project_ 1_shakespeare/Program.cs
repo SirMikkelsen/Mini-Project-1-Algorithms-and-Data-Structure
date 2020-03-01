8eb96acd9341cd8d92042493f88eaa699acbb41b
Gordon <gordonmikkelsen87@gmail.com>
2020-03-01 15:17:07 +0100

    added split

--- mini project_ 1_shakespeare/Program.cs
+++ mini project_ 1_shakespeare/Program.cs
@@ -25,10 +25,23 @@ namespace mini_project__1_shakespeare
                 using (StreamReader sr = new StreamReader("shakespeare.txt"))
                 {
                    
-                    string[] split = _line.Split();
+                  
                     while ((_line = sr.ReadLine()) != null)
                     {
-                        
+
+                        string[] split = _line.Split(" ");
+
+                        int i = 0;
+
+                        string word;
+
+                        char[] charsToTrim = { ',', '.', ';', ':' };
+
+                        while (word = split[i].Trim(charsToTrim)) 
+                        {
+                            i++;
+
+                        }
                         _list.Add(_line);
                         Console.WriteLine(_line);
                     }
