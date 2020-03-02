4fc70f3f4b30666239b2471f45d84524e2fc35f0
Gordon <gordonmikkelsen87@gmail.com>
2020-03-02 15:00:45 +0100

    added extra streamreader

--- mini project_ 1_shakespeare/Program.cs
+++ mini project_ 1_shakespeare/Program.cs
@@ -9,18 +9,18 @@ namespace mini_project__1_shakespeare
     class Program
     {
 
-       //public static string[] _list;
-       public static List<string> _list = new List<string>();
-       public static string _line;
-       public static SelectionSort<string> _mySort = new SelectionSort<string>();
-      
+        public static string[] _list;
+        //public static List<string> _list = new List<string>();
+        public static string _line;
+        public static string _srline;
+        public static SelectionSort<string> _mySort = new SelectionSort<string>();
+        // public static string[] _array = _list.ToArray();
+        public static int counter = 0;
 
 
         static void Main(string[] args)
         {
 
-            String[] array = _list.ToArray(); 
-            
             Console.WriteLine("pres 1 to run HeapSort , 2 Insertionsort, 3 Mergesort, 4 SelectionSort, 5 Trie");
 
             string selction = Console.ReadLine();
@@ -37,11 +37,14 @@ namespace mini_project__1_shakespeare
                     Console.WriteLine("Do Merge");
                     break;
                 case "4":
-                    string[] result = _mySort.Sort(array);
+                    string[] result = _mySort.Sort(_list);
+                    Console.WriteLine(result.Length);
                     for (int i = 0; i < result.Length; i++)
                     {
+                        Console.WriteLine("Test2");
                         Console.WriteLine(result[i]);
                     }
+                    Console.WriteLine("Tet3");
                     break;
                 case "5":
                     Console.WriteLine("Do trie");
@@ -49,7 +52,7 @@ namespace mini_project__1_shakespeare
                 default:
                     Console.WriteLine("out of range");
                     break;
-                    
+
             }
 
             Readfile();
@@ -57,14 +60,44 @@ namespace mini_project__1_shakespeare
         }
 
 
-
         public static void Readfile()
         {
             try
             {
 
+                using (StreamReader srcount = new StreamReader("../../../../shakespeare.txt"))
+                { 
+                    while((_srline = srcount.ReadLine()  ) != null)
+                    {
+                        string[] srsplit = _srline.Split(" ");
+                        int i = 0;
+
+                        char[] charsToTrim = { ',', '.', ';', ':' };
+
+                        foreach ( string srword in srsplit)
+                        {
+                            string srcword = srword.Trim(charsToTrim);
+
+
+                            if (srcword != null && srcword != "" && srcword != " ")
+                            {
+
+                                counter++;
+                            }
+
+                            i++;
+                        }
+
+                    }
+
+                    
+                }
+                Array.Resize(ref _list,counter);
+                Console.WriteLine(counter);
+
                 using (StreamReader sr = new StreamReader("../../../../shakespeare.txt"))
                 {
+
 
                     while ((_line = sr.ReadLine()) != null)
                     {
@@ -76,18 +109,18 @@ namespace mini_project__1_shakespeare
                         foreach (string word in split)
                         {
                             string cword = word.Trim(charsToTrim);
-                           
-                            if (cword != null && word != "" && word !=" ")
+
+                            if (cword != null && word != "" && word != " ")
                             {
-                                    _list.Add(cword);
-                                  //_list[] = cword 
+                                // _list.Add(cword);
+                                _list[i] = cword; 
 
                             }
 
                             i++;
 
                         }
-                      
+
                     }
                 }
 
